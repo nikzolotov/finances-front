bd140439ae9baa880816c08d6b8f1e5ac1fc72f8
Nikita Zolotov <nikzolotov@gmail.com>
2024-12-13 19:49:27 +0300

    Load expenses data from Strapi

--- src/app/routes/home.jsx
+++ src/app/routes/home.jsx
@@ -1,7 +1,45 @@
+import { useLoaderData } from "react-router-dom";
+import qs from "qs";
+
 export const homeLoader = async () => {
-  return {};
+  const expensesQuery = qs.stringify({
+    fields: ["date", "sum"],
+    populate: "category",
+  });
+  // const tagsQuery = qs.stringify({
+  //   sort: "name:asc",
+  //   filters: {
+  //     isMain: true,
+  //   },
+  // });
+  const [expensesResponse] = await Promise.all([
+    fetch(`${import.meta.env.VITE_STRAPI_API_URL}expenses?${expensesQuery}`),
+    // fetch(`${import.meta.env.VITE_STRAPI_API_URL}tags?${tagsQuery}`),
+  ]);
+  const [expensesData] = await Promise.all([
+    expensesResponse.json(),
+    // tagsResponse.json(),
+  ]);
+  return {
+    expenses: expensesData.data,
+    // observationsTotal: observationsData.meta.pagination.total,
+    // tags: tagsData.data,
+  };
 };
 
 export const HomeRoute = () => {
-  return <div>Home</div>;
+  const { expenses } = useLoaderData();
+  return <ExpensesTmp data={expenses} />;
+};
+
+const ExpensesTmp = ({ data }) => {
+  return (
+    <ul>
+      {data.map((expense) => (
+        <li key={expense.id}>
+          {expense.date} - {expense.sum} - {expense.category.name}
+        </li>
+      ))}
+    </ul>
+  );
 };
